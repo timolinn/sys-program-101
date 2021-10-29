e5608cc8df768240c34eca7179042c35839d8363
Timothy Onyiuke <timothyonyiuke@gmail.com>
2021-10-29 14:31:36 +0100

    cpu 3

--- src/lib/sysproglib.rs
+++ src/lib/sysproglib.rs
@@ -12,6 +12,8 @@ pub struct CPU {
     pub position_in_memory: usize,
     pub registers: [u8; 16],
     pub memory: [u8; 0x1000], // memory: [u8; 4096],
+    pub stack: [u16; 16],
+    pub stack_pointer: usize,
 }
 
 impl CPU {
@@ -40,6 +42,27 @@ impl CPU {
         }
     }
 
+    fn call(&mut self, addr: u16) {
+        let sp = self.stack_pointer;
+
+        if sp > self.stack.len() {
+            panic!("stack overflow")
+        }
+
+        self.stack[sp] = self.position_in_memory as u16;
+        self.stack_pointer += 1;
+        self.position_in_memory = addr as usize;
+    }
+
+    fn ret(&mut self) {
+        if self.stack_pointer == 0 {
+            panic!("Stack underflow");
+        }
+
+        self.stack_pointer -= 1;
+        self.position_in_memory = self.stack[self.stack_pointer] as usize;
+    }
+
     pub fn run(&mut self) {
         loop {
             let opcode = self.read_opcode();
@@ -50,10 +73,14 @@ impl CPU {
             let y = ((opcode & 0x00F0) >> 4) as u8;
             let d = ((opcode & 0x000F) >> 0) as u8;
 
+            let nnn = opcode & 0x0FFF;
+
             match (c, x, y, d) {
                 (0, 0, 0, 0) => {
                     return;
                 }
+                (0, 0, 0xE, 0xE) => self.ret(),
+                (0x2, _, _, _) => self.call(nnn),
                 (0x8, _, _, 0x4) => self.add_xy(x, y),
                 _ => todo!("opcode {:04x}", opcode),
             }
--- src/main.rs
+++ src/main.rs
@@ -17,26 +17,28 @@ fn main() {
         position_in_memory: 0,
         registers: [0; 16],
         memory: [0; 4096],
+        stack: [0; 16],
+        stack_pointer: 0,
     };
 
-    // 3. into u8 values into registers
     cpu.registers[0] = 5;
     cpu.registers[1] = 10;
-    cpu.registers[2] = 10;
-    cpu.registers[3] = 10;
 
-    cpu.memory[0] = 0x80;
-    cpu.memory[1] = 0x14;
-    cpu.memory[2] = 0x80;
-    cpu.memory[3] = 0x24;
-    cpu.memory[4] = 0x80;
-    cpu.memory[5] = 0x34;
+    cpu.memory[0x000] = 0x21;
+    cpu.memory[0x001] = 0x00;
+    cpu.memory[0x002] = 0x21;
+    cpu.memory[0x003] = 0x00;
+    cpu.memory[0x100] = 0x80;
+    cpu.memory[0x101] = 0x14;
+    cpu.memory[0x102] = 0x80;
+    cpu.memory[0x103] = 0x14;
+    cpu.memory[0x104] = 0x00;
+    cpu.memory[0x105] = 0xEE;
 
     cpu.run();
 
-    assert_eq!(cpu.registers[0], 35);
-
-    println!("5 + 10 + 10 + 10 = {}", cpu.registers[0]);
+    assert_eq!(cpu.registers[0], 45);
+    println!("5 + (10 * 2) + (10 * 2) = {}", cpu.registers[0]);
 }
 
 #[cfg(test)]
